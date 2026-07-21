5dee2d004e5566fecc3fa6721809906a85941cb0
My-projects <kashishvermav4@gmail.com>
2026-07-21 13:42:21 +0530

    updated

---
+++ js/contact.js
@@ -0,0 +1,59 @@
+emailjs.init("rbcYF-ftTP6MUowTJ");
+
+document.getElementById("contactForm").addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    let name = document.getElementById("name").value.trim();
+    let phone = document.getElementById("phone").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let service = document.getElementById("service").value;
+    let message = document.getElementById("message").value.trim();
+
+    if(name==="" || phone==="" || email==="" || service==="" || message===""){
+
+        alert("Please fill all details");
+
+        return;
+
+    }
+
+    let templateParams={
+
+        form_name:"Contact Form",
+
+        user_name:name,
+
+        user_email:email,
+
+        user_phone:phone,
+
+        message:
+        "Service : "+service+
+        "\n\nMessage : "+message
+
+    };
+
+    emailjs.send(
+        "service_5j3se2i",
+        "template_1fkywz5",
+        templateParams
+    )
+
+    .then(function(){
+
+        alert("Thank you! Your enquiry has been sent successfully.");
+
+        document.getElementById("contactForm").reset();
+
+    })
+
+    .catch(function(error){
+
+        console.log(error);
+
+        alert("Something went wrong.");
+
+    });
+
+});
